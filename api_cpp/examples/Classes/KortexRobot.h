09ff8d9f3e541a03831508ec67d41f05abb7f2fb
Harrison Gordon <19hmkg@queensu.ca>
2024-01-13 14:49:12 -0500

    syntax fixes on header file

--- api_cpp/examples/Classes/KortexRobot.h
+++ api_cpp/examples/Classes/KortexRobot.h
@@ -1,25 +1,30 @@
-#include <cstdlib>
+// KortexRobot.h
+#ifdef KORTEX_ROBOT_H
+#define KORTEX_ROBOT_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+
+#define PORT 10000
+#define PORT_RT 10001
 
 
 class KortexRobot
 {
 private:
     /* data */
+
 public:
     KortexRobot(/* args */);
     ~KortexRobot();
 
+    std::vector<std::vector<int>> read_csv(const std::string &filename);
+
 protected:
     std::string ip_address;
-
-
-    
-
 };
 
-KortexRobot::KortexRobot(/* args */);
+#endif // KORTEX_ROBOT_H
 
-KortexRobot::~KortexRobot();
-
-std::vector<std::vector<int>> read_csv(const std::string& filename);
