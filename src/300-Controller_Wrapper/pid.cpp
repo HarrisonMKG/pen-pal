8f33dfbf1552375d91187a58a8d0e3d614798986
Joaquin <joaquintola01@gmail.com>
2024-01-22 22:03:26 -0500

    Added PID class and setup functions required. Getting ready to integrate with wrapper_class

--- src/300-Controller_Wrapper/pid.cpp
+++ src/300-Controller_Wrapper/pid.cpp
@@ -1,26 +1,52 @@
 #include "pid.hpp"
 
+// I want to pass the actuator index so that it can reference its individual kp, ki, kd gains and then compute
 
-PID::calculate_pid(float val)
+// The dt term is from an example where it is called the "loop interval time" you multiply it to the error for the intergral
+// and you devide it by the difference of errors for the Derivative (the example value was 0.1). 
+
+PID::calculate_pid(float currentLocation, float setPoint, int actuator_index)
 {
-	//implementation
+	//PID Controller calculations
+
+	float error = setPoint - currentLocation;
+
+	// Proportional term
+	float P = k_p * error;
+
+	// Integral term
+	integral += error * dt;
+	float I = k_i * integral;
+
+	// Derivative term
+	derivative = (error - prevError) / dt;
+	float D = k_d * derivative; 
+
+	// Control signal after PID controller interference
+	float controlSignal = P + I + D;
+	
+	prevErr = error;
+
+	return controlSignal;
 }
 
-PID::PID(float k_p, float k_i, float k_d);
+PID::PID(float k_p, float k_i, float k_d, float dt);
 {
 	PID::k_p = k_p;
 	PID::k_i = k_i;
+	PID::k_d = k_d;
 	PID::k_d = k_d;
 }
 
 PID::~PID()
 {
-	//deconstructor
+	//deconstructor (would this involve setting the variables back to zero? or destroying the gain variables? look into it further)
 }
 
 void PID::set_direction(int direction)
 {
-	if(direction == -1 || direction == 1)
+	// Should only matter if the direction is reversed??
+	if(direction == -1) // || direction == 1)
 	{
 		PID::direction = direction;
 	}
--- src/300-Controller_Wrapper/pid.hpp
+++ src/300-Controller_Wrapper/pid.hpp
@@ -1,3 +1,6 @@
+#ifndef PID.HPP
+#define PID.HPP
+
 #include <string>
 #include <vector>
 #include <cmath>
@@ -7,18 +10,37 @@ namespace k_api = Kinova::Api;
 class PID
 {
 	private:
-		int direction;//Should only be -1 and 1
-	public:
+		int direction; //Should only be -1 and 1
+
+		// these should not be touched by the user and be consistent per actuator
 		float k_p;
 		float k_i;
 		float k_d;
+		float dt;
 
+		// Not sure if these need to be private
+		float integral;
+		float derivative;
+		float setPoint;
+		float prevErr;
+		float direction;
 
+		// Later could introduce a Max and a Min value that the output calculation must stay within (the controlSignal)
+		// float max; // example values of max was 100
+		// float min; // example values of min was -100
+		
+	public:
+		
 		void set_direction(int direction);
-		float calculate_pid(float val); // not sure how this would work, just example of what you could do
+		float calculate_pid(float currentLocation, float setPoint, int actuator_index);
 
-		PID(float k_p, float k_i, float k_d);
+		PID(float k_p, float k_i, float k_d, float dt);
 		~PID();
+
 	protected:
 		//data
+
+
 }
+
+#endif // PID.HPP
