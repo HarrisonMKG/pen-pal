fbb7895ed3845a4bd0d0e63714e11c79f41cce3b
Harrison Gordon <19hmkg@queensu.ca>
2024-02-04 18:27:49 -0500

    WIP: integrate testing into pen pal demo

--- src/300-Controller_Wrapper/100-Pen-Pal-Demonstration.cpp
+++ src/300-Controller_Wrapper/100-Pen-Pal-Demonstration.cpp
@@ -18,10 +18,19 @@ int main(int argc, char **argv)
     // pen_pal.mylogger.Log("Executing Read Csv using filename: " + coordinates_file, INFO);
   
     vector<vector<float>> matrix = pen_pal.read_csv(coordinates_file);
+    pen_pal.start_plot();
+
+    vector<vector<float>> matrix_subset(matrix);
+    for(auto &points : matrix_subset)
+    {
+      points = {points.begin() + 1, points.end() - 1};
+    }
+
+    pen_pal.plot(matrix_subset);
     // pen_pal.mylogger.Log("Read CSV complete");
     // pen_pal.mylogger.Log("Executing Move cartesian.", INFO);
 
-    pen_pal.move_cartesian(matrix);
+    //pen_pal.move_cartesian(matrix);
 
     return 0;
 }
